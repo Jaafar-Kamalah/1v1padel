5c9127d127a8529e425610620b3593b6f86c34a9
Jaafar Kamalah <jaaka770@student.liu.se>
2026-08-02 22:59:02 +0200

    feat: integrate tailwind and add global css theme setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import "./styles/global.css"
 import Facilities from "./pages/Facilities";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
