d978decd885afe40829f0a86039170d3b4ca3931
Jaafar Kamalah <jaaka770@student.liu.se>
2026-08-19 16:23:11 +0200

    feat: add Supabase signUp logic

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,17 +1,49 @@
 import "./index.css";
+import supabase from "../../lib/supabase";
+import { useState } from "react";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      console.error(error);
+      alert(error.message);
+    } else {
+      alert("Success");
+    }
+  }
+
   return (
     <>
       <div className="card">
-        <h1>Enter Your Email and a Password</h1>
-        <div className="input-group">
-          <input type="email" placeholder="Email"/>
-        </div>
-        <div className="input-group">
-          <input type="password" placeholder="Password" />
-        </div>
-        <button>Continue</button>
+        <form onSubmit={handleSubmit}>
+          <h1>Enter Your Email and a Password</h1>
+          <div className="input-group">
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Continue</button>
+        </form>
       </div>
     </>
   );
