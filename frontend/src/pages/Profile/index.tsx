852e10db843d61c1c79792cdd267ada0f6746b43
Jaafar Kamalah <jaaka770@student.liu.se>
2026-08-21 20:58:09 +0200

    implement basic logout functionality

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import "./index.css"
+import { useAuthContext } from "../../contexts/AuthContext";
+import supabase from "../../lib/supabase";
+
 function Profile() {
+  const [loggingOut, setLoggingOut] = useState(false);
+  
+  async function logout() {
+    setLoggingOut(true);
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      alert("Error during logout : " + error.message);
+      console.error("Error during logout : " + error.message);
+    }
+    setLoggingOut(false);
+  }
+
+  const {session} = useAuthContext();
+
   return (
     <>
-      <h1>Profile</h1>
+      <div className="card">
+        <h1>Profile</h1>
+        <p>Email: {session?.user.email}</p>
+        <button disabled={loggingOut} onClick={logout}>
+          {loggingOut ? "Logging out..." : "Log Out"}
+        </button>
+      </div>
     </>
   );
 }
