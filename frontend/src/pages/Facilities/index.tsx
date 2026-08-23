0d7fecb2454c4cf2879263231bdbefd15561efc8
Jaafar Kamalah <jaaka770@student.liu.se>
2026-08-23 11:20:08 +0200

    feat: implement myFacilities loading

--- frontend/src/pages/Facilities/index.tsx
+++ frontend/src/pages/Facilities/index.tsx
@@ -1,26 +1,53 @@
 import FacilityCard from "./FacilityCard";
 import supabase from "../../lib/supabase";
 import { useEffect, useState } from "react";
-import type { Database } from "../../../../supabase/types"
+import type { Database } from "../../../../supabase/types";
 import "./index.css";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 type Facility = Database["public"]["Tables"]["facilities"]["Row"];
 
 function Facilities() {
   // TODO: Implement loading myFacilities after setting up auth
-  const [myFacilities, setmyFacilities] = useState<Facility[]>([]);
+  const [myFacilities, setMyFacilities] = useState<Facility[]>([]);
   const [allFacilities, setAllFacilities] = useState<Facility[]>([]);
   const [loading, setLoading] = useState(true);
+  const { session } = useAuthContext();
 
   // Load facilites once after mount
   useEffect(() => {
     async function loadFacilities() {
-      const { data, error } = await supabase.from("facilities").select("*");
+      const userId = session?.user?.id;
 
-      if (error) {
-        console.error("Error fetching facilities: ", error);
+      const allFacilitiesPromise = supabase.from("facilities").select("*");
+
+      // Request joined facilities if session is valid
+      const myFacilitiesPromise = userId
+        ? supabase
+            .from("memberships")
+            .select("facilities(*)")
+            .eq("user_id", userId)
+        : Promise.resolve({ data: [], error: null });
+
+      const [allFacilitiesResult, myFacilitiesResult] = await Promise.all([
+        allFacilitiesPromise,
+        myFacilitiesPromise,
+      ]);
+
+      if (allFacilitiesResult.error) {
+        console.error("Error fetching facilities: ", allFacilitiesResult.error);
       } else {
-        setAllFacilities(data);
+        setAllFacilities(allFacilitiesResult.data);
+      }
+
+      if (myFacilitiesResult.error) {
+        console.error("Error fetching facilities: ", myFacilitiesResult.error);
+      } else {
+        const myFacilitiesData = myFacilitiesResult.data ?? [];
+        // Convert from [{facilities: {...}}, {facilities: {...}} to [{...}, {...}]
+        setMyFacilities(
+          myFacilitiesData.map((row: any) => row.facilities).filter(Boolean),
+        );
       }
       setLoading(false);
     }
