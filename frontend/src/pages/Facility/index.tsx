f98f5be7bafcf3a741a4338c9769b31ad41789df
Jaafar Kamalah <jaaka770@student.liu.se>
2026-08-25 09:45:29 +0200

    feat: implement fetching leaderboard

--- frontend/src/pages/Facility/index.tsx
+++ frontend/src/pages/Facility/index.tsx
@@ -4,13 +4,16 @@ import type { Database } from "../../../../supabase/types";
 import supabase from "../../lib/supabase";
 
 type Facility = Database["public"]["Tables"]["facilities"]["Row"];
+type LeaderboardEntry =
+  Database["public"]["Views"]["facility_leaderboard"]["Row"];
 
 function Facility() {
   const { facilityId } = useParams();
 
   const [facilityLoading, setFacilityLoading] = useState(true);
-  const [leaderboardLoading, setLeaderboardLoading] = useState(false);
+  const [leaderboardLoading, setLeaderboardLoading] = useState(true);
   const [facility, setFacility] = useState<Facility | null>(null);
+  const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
 
   useEffect(() => {
     async function loadFacility() {
@@ -37,6 +40,30 @@ function Facility() {
     loadFacility();
   }, [facilityId]);
 
+  useEffect(() => {
+    async function loadLeaderboard() {
+      setLeaderboardLoading(true);
+      if (!facilityId) {
+        setLeaderboardLoading(false);
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from("facility_leaderboard")
+        .select("*")
+        .eq("facility_id", facilityId);
+
+      if (error) {
+        console.error("Error fetching leaderboard: ", error);
+      } else {
+        setLeaderboard(data);
+      }
+      setLeaderboardLoading(false);
+    }
+
+    loadLeaderboard();
+  }, [facilityId]);
+
   if (facilityLoading || leaderboardLoading) {
     return <p>Loading page...</p>;
   } else if (!facilityLoading && !facility) {
@@ -46,6 +73,9 @@ function Facility() {
   return (
     <div className="facility-page">
       <h1>{facility?.name}</h1>
+      {leaderboard.map((l) => (
+        <p key={l.user_id}>{l.first_name}</p>
+      ))}
     </div>
   );
 }
