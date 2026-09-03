a694867f9256a423dc6afb7a0ad17e9185517213
Jaafar Kamalah <jaaka770@student.liu.se>
2026-09-03 17:39:32 +0200

    feat: implement basic challenges page scaffolding

--- frontend/src/pages/Challenges/index.tsx
+++ frontend/src/pages/Challenges/index.tsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+
 function Challenges() {
   return (
-    <>
-      <h1>Challenges</h1>
-    </>
+    <div className="challenges-page">
+      <div className="card">
+        <h1>All Challenges</h1>
+        <ul className="list">
+          <li className="row">
+            <Link className="link" to="/">
+              <span>Jaafar Kamalah</span>
+            </Link>
+          </li>
+          <li className="row">
+            <Link className="link" to="/">
+              <span>Hassan Ali</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
