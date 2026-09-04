65c3e2066417e6ee3d500c574e007a56c75635d9
Jaafar Kamalah <jaaka770@student.liu.se>
2026-09-04 18:49:09 +0200

    feat: implement loading challenges

--- frontend/src/pages/Challenges/index.tsx
+++ frontend/src/pages/Challenges/index.tsx
@@ -1,13 +1,45 @@
-import { Link } from "react-router-dom";
 import "./index.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Database } from "../../../../supabase/types";
 import ChallengeRow from "./ChallengeRow";
+import { useAuthContext } from "../../contexts/AuthContext";
+import supabase from "../../lib/supabase";
 
 type Challenge = Database["public"]["Tables"]["challenges"]["Row"];
 
 function Challenges() {
+  const { session } = useAuthContext();
+  const userId = session?.user?.id;
+
   const [challenges, setChallenges] = useState<Challenge[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function loadChallenges() {
+    if (!userId) {
+      setChallenges([]);
+      setLoading(false);
+      return;
+    }
+    
+    const { data, error } = await supabase
+      .from("challenges")
+      .select("*")
+      .or(`sender_user_id.eq.${userId}, receiver_user_id.eq.${userId}`);
+
+    if (error) {
+      console.error("Error fetching challenges: ", error);
+    } else {
+      setChallenges(data);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadChallenges();
+  }, [userId]);
+
+  if (loading)
+    return <p>Loading challenges...</p>; 
 
   return (
     <div className="challenges-page">
@@ -19,7 +51,7 @@ function Challenges() {
         ) : (
           <ul className="list">
             {challenges.map((c) => (
-              <ChallengeRow challenge={c}/>
+              <ChallengeRow key={c.id} challenge={c} />
             ))}
           </ul>
         )}
