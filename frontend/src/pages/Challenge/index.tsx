c1b9878513757cc1d99e00b1286aff1ca731f4cf
Jaafar Kamalah <jaaka770@student.liu.se>
2026-09-09 09:03:51 +0200

    style: add basic card layout

--- frontend/src/pages/Challenge/index.tsx
+++ frontend/src/pages/Challenge/index.tsx
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom";
+import "./index.css";
 
 function Challenge() {
   const { challengeId } = useParams();
 
-  return <p>Challenge {challengeId}</p>;
+  return (
+    <div className="challenge-page">
+      <div className="card">
+        <p>Challenge {challengeId}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Challenge;
