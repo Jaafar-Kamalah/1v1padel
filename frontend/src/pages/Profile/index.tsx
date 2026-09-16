17d9f6a1f739625c2b66b396ef081bd812651f8b
Jaafar Kamalah <jaaka770@student.liu.se>
2026-09-16 13:16:16 +0200

    feat: display all profile information

--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -52,11 +52,38 @@ function Profile() {
     <div className="profile-page">
       <div className="card">
         <h1>Profile</h1>
-        <p>Email: {session?.user.email}</p>
-        <button className="gb-red-btn" disabled={loggingOut} onClick={logout}>
-          {loggingOut ? "Logging out..." : "Log Out"}
-        </button>
+        <div className="field">
+          <span className="label">First Name:</span>{" "}
+          <span className="value">{profile?.first_name}</span>
+        </div>
+        <div className="field">
+          <span className="label">Last Name:</span>{" "}
+          <span className="value">{profile?.last_name}</span>
+        </div>
+        <div className="field">
+          <span className="label">Email:</span>{" "}
+          <span className="value">{session?.user.email}</span>
+        </div>
+        <div className="field">
+          <span className="label">Rating:</span>{" "}
+          <span className="value">{profile?.rating}</span>
+        </div>
+        <div className="field">
+          <span className="label">Join Date:</span>{" "}
+          <span className="value">
+            {profile?.created_at
+              ? new Date(profile.created_at).toLocaleDateString(undefined, {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })
+              : ""}
+          </span>
+        </div>
       </div>
+      <button className="gb-red-btn" disabled={loggingOut} onClick={logout}>
+        {loggingOut ? "Logging out..." : "Log Out"}
+      </button>
     </div>
   );
 }
